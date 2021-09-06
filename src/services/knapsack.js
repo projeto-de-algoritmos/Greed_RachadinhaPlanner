a5f3f8f9eec84c4cf332b6d2c29ea43978abb8ab
João Luis Baraky <jlbaraky@gmail.com>
2021-09-06 20:00:45 -0300

    Add knapsack algorithm.

---
+++ src/services/knapsack.js
@@ -0,0 +1,24 @@
+export function knapsack(departments, limit) {
+    var departsList = departments.sort((a, b) => {
+        return (b.value / b.weight) - (a.value / a.weight)
+    });
+    var earnings = 0;
+    var limitRemainder = limit;
+    var orderedList = [];
+
+    for (var i in departsList) {
+        orderedList.push({ 
+            department: departsList[i], 
+            amount: Math.min(limitRemainder, departsList[i].weight) 
+        });
+
+        earnings += Math.min(limitRemainder, departsList[i].weight) * (departsList[i].value / departsList[i].weight);
+
+        limitRemainder -= Math.min(limitRemainder, departsList[i].weight);
+    }
+
+    return { 
+        orderedDepartsList: orderedList, 
+        totalEarnings: earnings,
+    };
+}
